9495138d19a5526d4ee2f7442686d60fb2f5ffc1
aryaanand055 <aryaanand053@gmail.com>
2022-10-04 20:43:13 +0530

    Changed some lines of code

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
 const weather = new Weather;
 const ui = new Ui;
-let place;
 const cityInput = document.getElementById("floatingInput");
-
-
 
 window.addEventListener("load", function () {
     restoreFromLocalStorage(localStorage.getItem("city"));
 })
 
 cityInput.addEventListener("keyup", function (e) {
-
     const input = cityInput.value;
     if (e.keyCode === 13) {
-        document.getElementById("floatingInput").input = "";
-        place = input;
-
+        document.getElementById("floatingInput").value = "";
+        localStorage.setItem("city", input);
         set();
-        restoreFromLocalStorage(input);
-
-    } else {
-
     }
 })
 
@@ -36,11 +27,13 @@ document.addEventListener("click", function (e) {
 })
 
 function set() {
+    let place = localStorage.getItem("city")
     weather.fetchCurrentWeather(place).then(data => {
         if (data === "No Location found") {
             document.getElementById("city").innerText = "Location is not found or cannot be accessed";
             document.getElementById("city").classList.remove("text-light");
             document.getElementById("city").classList.add("text-warning");
+            ui.removeWeather();
             //             console.log(data);
         } else {
 
@@ -50,38 +43,30 @@ function set() {
     });
 }
 
-// restoreFromLocalStorage();
-
 function restoreFromLocalStorage(input = localStorage.getItem("city")) {
-    //     console.log(input);
     if (input !== null) {
-
         localStorage.setItem("city", input)
-        place = localStorage.getItem("city");
         set();
     } else {
         getGeo().then(d => {
-            place = d;
+            localStorage.setItem("city", d)
             set();
         })
 
     }
 
 }
+
 async function getGeo(token = "202b82ccc4d570") {
     let url = `https://ipinfo.io/json?token=${token}`;
     const response = await fetch(url);
     const data = await response.json();
-    getCity(data);
     return data.city;
-
 }
 
-function getCity(data) {
-    console.log(data.city)
-    return data.city
-}
-getGeo()
+
+
+// getGeo()
 
 //Include Navabar
 //Include Setting with 
--- ui.js
+++ ui.js
@@ -4,17 +4,23 @@ class Ui {
         this.img;
     }
     showWeather(weatherDetails, refresh = false) {
+        document.querySelector(".card-body").style.display = "block";
+        document.querySelector("#iconInfo").style.display = "block";
+        document.querySelector("#imgIcon").style.display = "block";
+        document.querySelector(".card-header").style.textAlign = "left";
+
+
         const time = this.getTime();
         const iconUrl = weatherDetails.current.condition.icon;
         document.getElementById("imgIcon").src = `https:${iconUrl}`;
         document.getElementById("iconInfo").innerText = `${weatherDetails.current.condition.text}`;
         document.getElementById("city").innerText = weatherDetails.location.name;
         this.card_container.innerHTML = `
-     <ul class="list-group">
-                        <li class="list-group-item border-top border-bootom rounded">Temperature: ${weatherDetails.current.temp_c} &#8451;</li>
-                        <li class="list-group-item border-top border-bootom rounded">Wind-Speed: ${weatherDetails.current.wind_kph} kph</li>
-                        <li class="list-group-item border-top border-bootom rounded">Visibility: ${weatherDetails.current.vis_km} km</li>
-                        <li class="list-group-item border-top border-bootom rounded">Humidity: ${weatherDetails.current.humidity} %</li>
+     <ul class="list-group text-light">
+                        <li class="list-group-item border-top border-bootom rounded text-light">Temperature: ${weatherDetails.current.temp_c} &#8451;</li>
+                        <li class="list-group-item border-top border-bootom rounded text-light">Wind-Speed: ${weatherDetails.current.wind_kph} kph</li>
+                        <li class="list-group-item border-top border-bootom rounded text-light">Visibility: ${weatherDetails.current.vis_km} km</li>
+                        <li class="list-group-item border-top border-bootom rounded text-light">Humidity: ${weatherDetails.current.humidity} %</li>
                     </ul>
                     <h6 class = "text-right text-small mt-2 font-italic"><small>Last Updated: ${time.year}-${time.month}-${time.date} ${time.hour}:${time.minutes}  <img id="refresh" src="refresh.png" width="15px" height="15px" class="ref" alt="Refresh Icon"></small></h6>
                      
@@ -22,6 +28,12 @@ class Ui {
       
         `
         this.refreshFun(refresh);
+    }
+    removeWeather() {
+        document.querySelector(".card-body").style.display = "none";
+        document.querySelector("#iconInfo").style.display = "none";
+        document.querySelector("#imgIcon").style.display = "none";
+        document.querySelector(".card-header").style.textAlign = "center";
     }
 
     refreshFun(e) {
